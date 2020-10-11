84f302bcf7fb42b7b2c2b3123a1c1d44d80a6be6
SungWonBin <mirr_x@naver.com>
2020-10-12 04:48:32 +0900

    CameraProcessFix

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,35 +1,69 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class CameraScript : MonoBehaviour
 {
 
-    public GameObject Player;
-    private GameObject Target;
-    public float CameraZ = -10;
 
-    // Start is called before the first frame update
+    public float smoothTimeX, smoothTimeY;
+
+    public Vector2 velocity;
+
+    public GameObject player;
+
+    public Vector2 minPos, maxPos;
+
+    public bool bound;
+
+
+
+    // 캐릭터 초기화
+
     void Start()
     {
-        
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
-    // Update is called once per frame
-    /*
+
+    // 캐릭터의 위에 따라 카메라가 이동하도록 하는 메서드
+
     void FixedUpdate()
     {
-        Vector3 TargetPos = new Vector3(Target.transform.position.x, Target.transform.position.y, CameraZ);
-        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 2f);
-    }
-    */
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if(col.gameObject.CompareTag("Portal"))
+
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+
+        // Mathf.SmoothDamp는 천천히 값을 증가시키는 메서드이다.
+
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        // 카메라 이동
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+
+        if (bound)
         {
-            Debug.Log("HI");
-            
+
+            //Mathf.Clamp(현재값, 최대값, 최소값);  현재값이 최대값까지만 반환해주고 최소값보다 작으면 그 최소값까지만 반환합니다.
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x),
+
+                Mathf.Clamp(transform.position.y, minPos.y, maxPos.y),
+
+                Mathf.Clamp(transform.position.z, transform.position.z, transform.position.z)
+
+            );
+
         }
+
     }
+
 }
+
+
