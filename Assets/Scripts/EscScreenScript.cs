dfd015a7d559c58d8df181ee12264199a8ed458f
SungWonBin <mirr_x@naver.com>
2021-05-19 16:26:45 +0900

    adding option screen demo

---
+++ Assets/Scripts/EscScreenScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscScreenScript : MonoBehaviour
+{
+
+    private RectTransform rectTransform;
+    private float rectCtrlX = 0f;
+    private float rectCtrlY = -1100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition = new Vector2(rectCtrlX, rectCtrlY);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+            if(rectCtrlY <= -200)
+                rectCtrlY += 50;
+
+        if (Input.GetKey(KeyCode.Alpha2))
+            if (rectCtrlY >= -1100)
+                rectCtrlY -= 50;
+    }
+}
