86067759837c9546dba012475eabed2b9d6a6b20
SungWonBin <mirr_x@naver.com>
2021-11-24 14:46:49 +0900

    Adding Stage Name

--- Assets/Scripts/StageNameScript.cs
+++ Assets/Scripts/StageNameScript.cs
@@ -1,57 +1,117 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class StageNameScript : MonoBehaviour
 {
-    public bool isHelper = false;
-
     private RectTransform rt;
 
-    private TextMeshProUGUI tmp;
+    private float temp = 700f;
 
-    //private float myWhite = 0;
-
-    //private float timer = 0f;
-
-    private Color clr;
+    private float timer = 0;
+    private float timer2 = 0;
+    private float timer3 = 0;
+    private float timer4 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rt = gameObject.GetComponent<RectTransform>();
-        tmp = gameObject.GetComponent<TextMeshProUGUI>();
 
-        rt.sizeDelta = new Vector2(rt.sizeDelta.x, rt.sizeDelta.y);
         rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, rt.anchoredPosition.y);
-
-        if (isHelper == false)
-        {
-
-
-
-
-
-        }
-        else
-        {
-
-
-        }
 
 
 
     }
 
+    private void FixedUpdate()
+    {
+        SetFirstTitle();
+        SetForestTimer();
+        SetForest2Timer();
+        SetHillTimer();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, temp);
+
+        if(PlayerScript.map1 == true)
+            SetFirstPos();
+
+        //어두운 숲
+        if (PlayerScript.map1 == false && GameManager.isReady2 == false && timer2 < 5.0f && temp >= 350f)
+                temp -= 15f;
+        else if (timer2 >= 5.0f && temp < 700f && timer2 < 10f)
+            temp += 15f;
+
+        //숲의 출구
+        if (GameManager.isReady2 == false && PortalScript.portal2Checker == true && timer3 < 4.0f && temp >= 350f)
+            temp -= 15f;
+        else if (timer3 >= 4.0f && temp < 700f && timer3 < 7f)
+            temp += 15f;
+
+        // 황무지
+        if (GameManager.isReady2 == true && BossEvent.finishBoss == false && timer4 < 4.0f && temp >= 350f)
+            temp -= 15f;
+        else if (timer4 >= 4.0f && temp < 700f && timer4 < 7f)
+            temp += 15f;
+
+        Debug.Log(timer3);
+        //Debug.Log(timer4);
+    }
+
+    public void SetFirstPos()
+    {
+        if (temp >= 350 && Tuto.setFirst == true && PlayerScript.map1 == true && timer < 3.0f)
+        {
+            temp -= 15f;
+        }
 
 
+        if (timer >= 3.0f && temp < 700f)
+            temp += 15f;
 
+    }
 
+    public void SetFirstTitle()
+    {
 
+        if (Tuto.setFirst == true && PlayerScript.map1 == true)
+            timer += Time.deltaTime;
 
+    }
+
+    public void SetForestTimer()
+    {
+        if (PlayerScript.map1 == false && GameManager.isReady2 == false)
+            timer2 += Time.deltaTime;
+
+    }
+    public void SetForest2Timer()
+    {
+        if (GameManager.isReady2 == false && PortalScript.portal2Checker == true)
+            timer3 += Time.deltaTime;
+
+    }
+
+    public void SetHillTimer()
+    {
+        if (GameManager.isReady2 == true && BossEvent.finishBoss == false)
+            timer4 += Time.deltaTime;
+
+    }
+
+    public void SetForestPos()
+    {
+        if (temp >= 350 && timer2 >= 3.0f)
+            temp -= 15f;
+        else if (temp <= 700f)
+            temp += 15f;
+
+        //Debug.Log(timer2);
     }
 }
---
+++ Assets/StageNameText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StageNameText : MonoBehaviour
+{
+    private TextMeshProUGUI tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerScript.map1 == false && GameManager.isReady2 == false)
+            tmp.text = "어두운 숲";
+
+        if(GameManager.isReady2 == false && PortalScript.portal2Checker == true)
+            tmp.text = "숲의 출구";
+
+        if (GameManager.isReady2 == true && BossEvent.finishBoss == false)
+            tmp.text = "황무지";
+
+
+    }
+}
